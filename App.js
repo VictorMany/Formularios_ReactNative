b563af7e4825c7a6cbdeff52ba1f571bc247addc
VictorMany <victor.velazfuentez@gmail.com>
2021-03-20 12:27:28 -0600

    hola

--- App.js
+++ App.js
@@ -1,112 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { LogBox, StyleSheet, SafeAreaView, StatusBar, View, Text } from "react-native";
+import React, { useState, useEffect } from 'react';
+import Form from './src/components/Form';
+import Footer from './src/components/Footer';
+import colors from './src/utils/Colors';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+LogBox.ignoreAllLogs();
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [cantidad, setCantidad] = useState(0);
+  const [interes, setInteres] = useState(0);
+  const [meses, setMeses] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+
+  }, [cantidad, interes, meses]);
+
+  const calculate = () => {
+    if (!cantidad) {
+      setErrorMessage('No has ingresado ninguna cantidad');
+    } else if (!interes) {
+      setErrorMessage('añade los intereses');
+    } else if (!meses) {
+      setErrorMessage('añade los meses');
+    } else {
+      const i = interes / 100;
+      const total = cantidad * (1 + i);
+      setTotal(total);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.SafeAreaView}>
+        <View style={styles.background} />
+        <Text style={styles.titleApp}>Prestamos</Text>
+        <Form />
+      </SafeAreaView>
+      <Footer />
+    </>
+  )
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  SafeAreaView: {
+    height: 290,
+    alignItems: "center",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  background: {
+    backgroundColor: colors.PRIMARY_COLOR,
+    height: 200,
+    width: '100%',
+    position: 'absolute',
+    zIndex: -1
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  titleApp: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#000',
+    marginTop: 15
+  }
+})
